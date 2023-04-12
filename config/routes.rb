6e0ed853fa29909bad64c1ee3f05aebb4ea3580a
Andrew Bowden <andrew.bowden@minigeek.org>
2023-04-12 14:42:49 +1000

    Added the git commit
    Commit the git to add the entries for CORS for testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/cloudfront_cached'
+  get 'static_pages/corstest'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
